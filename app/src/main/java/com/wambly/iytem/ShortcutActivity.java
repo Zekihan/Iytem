160a247b1a5df37115ff7d524d9caf4af66d65af
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2019-03-20 19:30:07 +0300

    .

--- app/src/main/java/com/wambly/iytem/ShortcutActivity.java
+++ app/src/main/java/com/wambly/iytem/ShortcutActivity.java
@@ -5,14 +5,13 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 public class ShortcutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_short_cut);
+        setContentView(R.layout.activity_shortcut);
 
         View obs = findViewById(R.id.obs);
         obs.setOnClickListener(new View.OnClickListener() {
