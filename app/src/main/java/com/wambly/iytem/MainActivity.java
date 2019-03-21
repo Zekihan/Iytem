c7c860b580595ce72f268ec2b66247b8a5c6463e
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2019-03-22 00:11:33 +0300

    .

--- app/src/main/java/com/wambly/iytem/MainActivity.java
+++ app/src/main/java/com/wambly/iytem/MainActivity.java
@@ -1,10 +1,25 @@
 package com.wambly.iytem;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        saveMenu();
 
         View announcements = findViewById(R.id.announcements);
         announcements.setOnClickListener(new View.OnClickListener() {
@@ -62,10 +79,42 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),ContactsActivity.class));
             }
         });
+    }
 
+    private void saveMenu(){
 
+        Calendar c = Calendar.getInstance();
+        long m = (c.get(Calendar.HOUR)*60*60*1000)
+                +(c.get(Calendar.MINUTE)*60*1000)
+                +(c.get(Calendar.SECOND)*1000)
+                +(c.get(Calendar.MILLISECOND));
+        long time = c.getTimeInMillis();
+        final long day = time-m+30681000-43200000;
+        final String d = String.valueOf(day);
+        Log.e("Food",""+(1553189481-1553146281));
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = mDatabase.getReference().child("food").child("refectory").child(d.substring(0,10));
 
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final String menu = dataSnapshot.getValue(String.class);
 
+                FileOutputStream outputStream;
+                try {
+                    outputStream = openFileOutput("menu.txt", Context.MODE_PRIVATE);
+                    outputStream.write(menu.getBytes());
+                    outputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                //Log.e("Food",menu);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
