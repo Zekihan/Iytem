e4f7b2430e79fc02fe23696b9978b14103e44e06
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2019-03-24 15:55:02 +0300

    custom tab warm up

--- app/src/main/java/com/wambly/iytem/FoodActivity.java
+++ app/src/main/java/com/wambly/iytem/FoodActivity.java
@@ -1,9 +1,16 @@
 package com.wambly.iytem;
 
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
+import android.support.customtabs.CustomTabsCallback;
+import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -20,6 +27,7 @@ public class FoodActivity extends AppCompatActivity {
     private CustomTabsIntent.Builder intentBuilder;
 
     private Toolbar toolbar;
+    private CustomTabsSession session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +40,28 @@ public class FoodActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
+            @Override
+            public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient customTabsClient) {
+                customTabsClient.warmup(1);
+                CustomTabsCallback customTabsCallback = new CustomTabsCallback();
+                session = customTabsClient.newSession(customTabsCallback);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        };
+        CustomTabsClient.bindCustomTabsService(this,"custom.tabs", connection);
+
         intentBuilder = new CustomTabsIntent.Builder();
         intentBuilder.setStartAnimations(this,R.anim.slide_in_right , R.anim.slide_out_left);
         intentBuilder.setExitAnimations(this, android.R.anim.slide_in_left,
                 android.R.anim.slide_out_right);
+        intentBuilder.setToolbarColor(Color.parseColor("#3949AB"));
+
+
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/wambly/iytem/ShortcutActivity.java
+++ app/src/main/java/com/wambly/iytem/ShortcutActivity.java
@@ -1,8 +1,15 @@
 package com.wambly.iytem;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
+import android.support.customtabs.CustomTabsCallback;
+import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsService;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +18,7 @@ import android.view.View;
 public class ShortcutActivity extends AppCompatActivity {
     private CustomTabsIntent.Builder intentBuilder;
     private Toolbar toolbar;
+    CustomTabsSession session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +31,30 @@ public class ShortcutActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        intentBuilder = new CustomTabsIntent.Builder();
+
+        CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
+            @Override
+            public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient customTabsClient) {
+                customTabsClient.warmup(1);
+                CustomTabsCallback customTabsCallback = new CustomTabsCallback();
+                 session = customTabsClient.newSession(customTabsCallback);
+
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        };
+        CustomTabsClient.bindCustomTabsService(this,"custom.tabs", connection);
+
+
+
+        intentBuilder = new CustomTabsIntent.Builder(session);
         intentBuilder.setStartAnimations(this,R.anim.slide_in_right , R.anim.slide_out_left);
         intentBuilder.setExitAnimations(this, android.R.anim.slide_in_left,
                 android.R.anim.slide_out_right);
+        intentBuilder.setToolbarColor(Color.parseColor("#3949AB"));
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,5 +111,11 @@ public class ShortcutActivity extends AppCompatActivity {
     private void chromeTab(String url){
         CustomTabsIntent customTabsIntent = intentBuilder.build();
         customTabsIntent.launchUrl(this, Uri.parse(url));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
     }
 }
