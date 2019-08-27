dee1ccc55f7c9d1d04727bfe2ddc22628f1cf693
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2019-08-27 22:26:34 +0300

    email and dial intent

--- app/src/main/java/com/wambly/iytem/ContactsDetailsActivity.java
+++ app/src/main/java/com/wambly/iytem/ContactsDetailsActivity.java
@@ -1,7 +1,11 @@
 package com.wambly.iytem;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
@@ -34,7 +38,7 @@ public class ContactsDetailsActivity extends AppCompatActivity {
 
         TextView name = findViewById(R.id.name);
         TextView email = findViewById(R.id.email);
-        TextView phone = findViewById(R.id.phone);
+        final TextView phone = findViewById(R.id.phone);
         TextView department = findViewById(R.id.department);
         TextView title = findViewById(R.id.title);
         name.setText(contact.getName());
@@ -42,6 +46,47 @@ public class ContactsDetailsActivity extends AppCompatActivity {
         phone.setText(String.format("%s: \n%s", getString(R.string.phone), contact.getPhone()));
         title.setText(String.format("%s: \n%s", getString(R.string.title), contact.getTitle()));
         department.setText(String.format("%s: \n%s", getString(R.string.department), contact.getDepartment()));
+
+        View emailView = findViewById(R.id.send_email);
+        emailView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailStr = contact.getEmail();
+                if(emailStr.contains("@")){
+                    sendEmail(contact.getEmail());
+                }
+            }
+        });
+
+        View phoneView = findViewById(R.id.call);
+        phoneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneStr = contact.getPhone();
+                if(phoneStr.replaceAll("\\D", "").length() >= 7) {
+                    phoneStr = phoneStr.split(":")[1];
+                    if ((!phoneStr.contains("232")) && ((phoneStr.charAt(0) != '5') ||
+                            ((phoneStr.charAt(0) != '0') && (phoneStr.charAt(1) != '5')))) {
+                        dialNum("0232" + phoneStr);
+                    }
+                }
+            }
+        });
+
+    }
+
+    private void sendEmail(String adress) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri data = Uri.parse("mailto:"+ adress);
+        intent.setData(data);
+        startActivity(intent);
+    }
+
+    private void dialNum(String num){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        String uri = "tel:" + num ;
+        intent.setData(Uri.parse(uri));
+        startActivity(intent);
 
     }
 }
